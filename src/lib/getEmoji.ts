6831c8dd8efc01549e7d7cbc0c39e0d167fe7eac
Tim Hagn <mail@timhagn.com>
2023-06-22 17:10:39 +0200

    fine tuned getEmoji()

--- src/lib/getEmoji.ts
+++ src/lib/getEmoji.ts
@@ -7,11 +7,14 @@ export function getEmoji(
       return '☠️'
     case numberOfTokes > pastNumberOfTokes:
       return '🤬'
+    case numberOfTokes > pastNumberOfTokes - 2:
+      return '🥵'
     case numberOfTokes === pastNumberOfTokes:
       return '🥺'
-    case numberOfTokes > Math.round(pastNumberOfTokes / 2) - 2 &&
-      numberOfTokes < Math.round(pastNumberOfTokes / 2) + 2:
+    case numberOfTokes >= Math.round(pastNumberOfTokes / 2) - 2:
       return '🤔'
+    case numberOfTokes <= Math.round(pastNumberOfTokes / 2) + 2:
+      return '😳'
     case numberOfTokes < pastNumberOfTokes:
     default:
       return '🙂'

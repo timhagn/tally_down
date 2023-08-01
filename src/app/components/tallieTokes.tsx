b3ffef0f783e34c69223ee6ed72733eba7d57f1d
Tim Hagn <mail@timhagn.com>
2023-08-01 14:01:02 +0000

    switch from yesterday's toke count to medianCount

--- src/app/components/tallieTokes.tsx
+++ src/app/components/tallieTokes.tsx
@@ -3,20 +3,22 @@
 import { loadPastPuffs, loadTodayPuffs, TallyTokes } from '@/lib/sqliteDb'
 import UpdateTallieTokes from '@/app/components/updateTallieTokes'
 import PastTokes from '@/app/components/pastTokes'
+import { calculateMedian } from '@/lib/meanUtils'
 
 export default async function TallieTokes() {
   const { numberOfTokes, lastTokeAt } = await loadTodayPuffs()
   const pastLoadedTokesResult = await loadPastPuffs()
   const pastTokesResult = pastLoadedTokesResult?.sort(
-    (a: TallyTokes, b: TallyTokes) => Date.parse(a.id) - Date.parse(b.id)
+    (a: TallyTokes, b: TallyTokes) => Date.parse(a.id) - Date.parse(b.id),
   )
-  const pastNumberOfTokes =
-    pastTokesResult?.[pastTokesResult?.length - 1]?.numberOfTokes || 0
+  // const pastNumberOfTokes =
+  //   pastTokesResult?.[pastTokesResult?.length - 1]?.numberOfTokes || 0
+  const medianTokes = calculateMedian(pastTokesResult)
   return (
     <div className="w-full md:w-1/2">
       <UpdateTallieTokes
         numberOfTokes={numberOfTokes}
-        pastNumberOfTokes={pastNumberOfTokes}
+        pastNumberOfTokes={medianTokes}
         lastTokeAt={lastTokeAt}
       />
       <PastTokes pastTokesResult={pastTokesResult} />
